6374d13b21fb61f123121e8e968c97e303ece2b9
Patricia Petrut <patricia.petrut@sap.com>
2018-03-01 20:56:04 +0200

    Merge branch 'master' of https://github.com/Patricia008/Google-HashCode

--- HashcodeChallenge/src/dataStructures/Route.java
+++ HashcodeChallenge/src/dataStructures/Route.java
@@ -1,57 +1,33 @@
 package dataStructures;
-public class Route {
-	private int rStart;
-	private int rEnd;
-	private int cStart;
-	private int cEnd;
-	private int tStart;
-	private int tEnd;
 
-	public int getrStart() {
-		return rStart;
-	}
+public class Route
+{
+    private int rStart;
+    private int rEnd;
+    private int cStart;
+    private int cEnd;
+    private int tStart;
+    private int tEnd;
 
-	public void setrStart(int rStart) {
-		this.rStart = rStart;
-	}
+    public int getrStart() { return rStart; }
+    public void setrStart(int rStart) { this.rStart = rStart; }
 
-	public int getrEnd() {
-		return rEnd;
-	}
+    public int getrEnd() { return rEnd; }
+    public void setrEnd(int rEnd) { this.rEnd = rEnd; }
 
-	public void setrEnd(int rEnd) {
-		this.rEnd = rEnd;
-	}
+    public int getcStart() { return cStart; }
+    public void setcStart(int cStart) { this.cStart = cStart; }
 
-	public int getcStart() {
-		return cStart;
-	}
+    public int getcEnd() { return cEnd; }
+    public void setcEnd(int cEnd) { this.cEnd = cEnd; }
 
-	public void setcStart(int cStart) {
-		this.cStart = cStart;
-	}
+    public int gettStart() { return tStart; }
+    public void settStart(int tStart) { this.tStart = tStart; }
 
-	public int getcEnd() {
-		return cEnd;
-	}
+    public int gettEnd() { return tEnd; }
+    public void settEnd(int tEnd) { this.tEnd = tEnd; }
 
-	public void setcEnd(int cEnd) {
-		this.cEnd = cEnd;
-	}
-
-	public int gettStart() {
-		return tStart;
-	}
-
-	public void settStart(int tStart) {
-		this.tStart = tStart;
-	}
-
-	public int gettEnd() {
-		return tEnd;
-	}
-
-	public void settEnd(int tEnd) {
-		this.tEnd = tEnd;
-	}
+    public int getTotalSteps() {
+        return Math.abs(rEnd - rStart) + Math.abs(cEnd - cStart);
+    }
 }
