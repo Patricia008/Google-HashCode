88d6527a5fcf81dd57d3806341321f1b36b74157
melorian94 <petra.csereoka@gmail.com>
2018-03-01 20:22:59 +0200

    datastructs

---
+++ HashcodeChallenge/src/InputParser.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class InputParser
+{
+    private String filepath = "";
+
+    public InputParser(String filepath)
+    {
+        this.filepath = filepath;
+    }
+
+    public void parse()
+    {
+        try
+        {
+            FileInputStream inputStream =  new FileInputStream(filepath);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+
+            // read parameters
+            line = reader.readLine();
+
+            if(line != null)
+            {
+                String[] parts = line.split(" ");
+
+            }
+
+            while((line = reader.readLine()) != null)
+            {
+
+            }
+
+            reader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
